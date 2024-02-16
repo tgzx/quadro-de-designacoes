f5dc140593bd40ad7130b9f8e4ca7973bdcc8ae0
tgzx • Tiago Moreira <tiago.z.x.g@gmail.com>
2024-02-16 17:37:09 -0300

    add user name on title head; add same checkbox into assignment screen; save in local storage the logged username;

--- assignment-card.js
+++ assignment-card.js
@@ -6,6 +6,7 @@ savedTheme = '';
 nameFound = false;
 designationsMiddleWeek = '';
 designationsEndWeek = '';
+waitLoadTime = 1500;
 
 
 function showLoading() {
@@ -17,22 +18,41 @@ function hideLoading() {
 
 function checkBoxChange(){
     const checkbox = document.getElementById('checkBoxWeek').checked;
-    onlyWeekPresentation = checkbox;
-    console.log('onlyWeekPresentation => ' + onlyWeekPresentation);
+    const checkbox2 = document.getElementById('checkBoxWeek2').checked;
+    let idSectionName = getActiveSection();
+    if(idSectionName === 'assigmentsSection'){
+        onlyWeekPresentation = checkbox2;
+        document.getElementById('checkBoxWeek').checked = checkbox2;
+    } else if(idSectionName === 'loginSection'){
+        onlyWeekPresentation = checkbox;
+        document.getElementById('checkBoxWeek2').checked = checkbox;
+    } else {
+        onlyWeekPresentation = checkbox;
+    }
+}
+
+function getActiveSection(){
+    divsComDisplayBlock = document.querySelectorAll('div[style*="display: block"]');
+    let idName = 'not found';
+    divsComDisplayBlock.forEach(div => {
+        idName = div.id;
+    });
+    return idName;
 }
 
 function search() {
     let name = document.getElementById('inputName').value;
-
+        
     if (name !== "" && name !== undefined) {
         hideElementLoad('loginSection');
         showElementLoad('assigmentsSection');
         showElementLoad('buttonBack');
-        readAssignmentsBD(name);
+        //readAssignmentsBD(name);
+        updateUserLogged();
         document.getElementById('inputName').value = "";
     } else {
         alert("Por favor, digite seu nome antes de buscar.");
-    }
+    }    
 }
 
 function backButton(home) {
@@ -46,6 +66,7 @@ function backButton(home) {
         changeTextById('meioDeSemana-assignment', 'Nenhuma designação encontrada.'); 
         changeTextById('fimDeSemana-assignment', 'Nenhuma designação encontrada.');
         activeFocusElement('inputName', 1800);
+        changeTextById('head-title', '');
     }
 }
 
@@ -66,7 +87,7 @@ function showElementLoad(element){
     setTimeout(function() {
         document.getElementById(element).style.display = 'block';
         hideLoading();
-    }, 1500);
+    }, waitLoadTime);
 }
 function hideElement(element){
     document.getElementById(element).style.display = 'none';
@@ -93,26 +114,6 @@ function changeCSSById(elementId, styleProperty, value) {
         console.error(`Elemento com ID '${elementId}' não encontrado.`);
     }
 }
-
-function getPresenter() {
-    // let name = document.getElementById('inputNameAssign').value;
-    // //console.log('name => ' + name);
-    const bdJSONPath = 'data/bd-accounts.json';
-    let bdJSON = {};
-
-    fetch(bdJSONPath)
-    .then(response => response.json())
-    .then(data => {
-        //console.log('Conteúdo do bdJSON:');
-        //console.log(data);
-
-        data.accounts.forEach(account => {
-            presenters = presenters.concat(account.name);
-        });
-
-        //console.log('presenters => ' + JSON.stringify(presenters));
-    });
-} 
 
 function readAssignmentsBD(name) {
     const bdJSONPath = 'data/bd.json';
@@ -216,12 +217,6 @@ function findDesignations(week, name){
     })
 }
 
-// function sameWeek(date1, date2) {
-//     const oneWeek = 7 * 24 * 60 * 60 * 1000; // milissegundos em uma semana
-//     const diff = Math.abs(date1 - date2);
-  
-//     return diff < oneWeek;
-// }
 function sameWeek(date1, date2) {
     const diffInDays = Math.abs(Math.floor((date1 - date2) / (24 * 60 * 60 * 1000)));
 
@@ -385,25 +380,24 @@ function toggleTheme() {
 }
 
 document.addEventListener('DOMContentLoaded', function() { // como se fosse o ConnectedCallBack
-    //console.log('O DOM foi completamente carregado.');
     showElementLoad('loginSection');
     today = new Date();
     // today.setDate(today.getDate() + 2); pra teste
     changeTextById('week-info', dateFormatter(today));
-    getPresenter(); // Chama a função para preencher presenters
-    setTimeout(() => { // Aguarda um curto período antes de chamar a função autocomplete
-        autocomplete(document.getElementById("inputNameAssign"), presenters);
-        autocomplete(document.getElementById("inputName"), presenters);
-    }, 1000); // Você pode ajustar o tempo de espera conforme necessário
     // Carrega a preferência de tema salva no localStorage
     savedTheme = localStorage.getItem('theme');
+    userLogged = localStorage.getItem('userLogged');
+    console.log('userLogged' + userLogged);
     if (savedTheme) {
         applyTheme(savedTheme);
     } else {
         // Se não houver preferência salva, aplica o tema com base na escolha do navegador
         updateDarkMode();
     }
-
+    if (userLogged) {
+        console.log('userLogged?' + userLogged);
+        loadInfoUser(userLogged);
+    }
 
     // Adiciona um evento de clique ao botão de tema
     const buttonDarkMode = document.getElementById('buttonDarkMode');
@@ -411,27 +405,24 @@ document.addEventListener('DOMContentLoaded', function() { // como se fosse o Co
         buttonDarkMode.addEventListener('click', toggleTheme);
     }
 
-    activeFocusElement('inputName', 1800);
-
-    document.getElementById('inputName').addEventListener('keyup', function(event) {
-        if(event.keyCode != '40' && event.keyCode != '38'){ // se for diferente de botao baixo e botao cima entra
-            simulateKey('inputName', '40');
-        }
-
-        setTimeout(() => {
-            if (event.key === 'Enter') {
-                search();
-            }
-        }, 300);
-    });
-
-    document.getElementById('inputNameAssign').addEventListener('keyup', function(event) {
-        if(event.keyCode != '40' && event.keyCode != '38'){ // se for diferente de botao baixo e botao cima entra
-            simulateKey('inputNameAssign', '40');
-        }
-    });
+    activeFocusElement('inputName', 1600);
 });
-  
+
+function updateUserLogged() {
+    let name = document.getElementById('inputName').value;
+    localStorage.setItem('userLogged', name);
+    setTimeout(() => {    
+        changeTextById('head-title', name);
+    }, waitLoadTime);
+}
+function loadInfoUser(userName){
+    let name = document.getElementById('inputName').value = userName;
+    setTimeout(() => {    
+        search();
+        changeTextById('head-title', name);
+    }, waitLoadTime);
+}
+
 function updateDarkMode() {
     //verifica se o navegador esta no modo escuro
     const loadingImage = document.getElementById('loadingImage');
@@ -528,111 +519,6 @@ function adminLogin(){
     }
 }
 
-let presenters = [];
-
-function autocomplete(inp, arr) {
-    //console.log('presenters 2=> ' + JSON.stringify(arr));
-    /*the autocomplete function takes two arguments,
-    the text field element and an array of possible autocompleted values:*/
-    var currentFocus;
-    /*execute a function when someone writes in the text field:*/
-    inp.addEventListener("input", function(e) {
-        var a, b, i, val = this.value;
-        /*close any already open lists of autocompleted values*/
-        closeAllLists();
-        if (!val) { return false;}
-        currentFocus = -1;
-        /*create a DIV element that will contain the items (values):*/
-        a = document.createElement("DIV");
-        a.setAttribute("id", this.id + "autocomplete-list");
-        a.setAttribute("class", "autocomplete-items");
-        /*append the DIV element as a child of the autocomplete container:*/
-        this.parentNode.appendChild(a);
-        /*for each item in the array...*/
-        // Limitando a exibição aos primeiros 5 países
-        var count = 0;
-        let isFind = false;
-        for (i = 0; i < arr.length && count < 5; i++) {
-            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-                b = document.createElement("DIV");
-                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-                b.innerHTML += arr[i].substr(val.length);
-                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-
-                b.addEventListener("click", function(e) {
-                    inp.value = this.getElementsByTagName("input")[0].value;
-                    closeAllLists();
-                });
-
-                a.appendChild(b);
-                count++;
-                isFind = true;
-            }
-        }
-
-        if(isFind){
-            hideElement('newPresenter');
-        } else {
-            showElement('newPresenter');
-        }
-    });
-    /*execute a function presses a key on the keyboard:*/
-    inp.addEventListener("keydown", function(e) {
-        var x = document.getElementById(this.id + "autocomplete-list");
-        if (x) x = x.getElementsByTagName("div");
-        if (e.keyCode == 40) {
-        /*If the arrow DOWN key is pressed,
-        increase the currentFocus variable:*/
-        currentFocus++;
-        /*and and make the current item more visible:*/
-        addActive(x);
-        } else if (e.keyCode == 38) { //up
-        /*If the arrow UP key is pressed,
-        decrease the currentFocus variable:*/
-        currentFocus--;
-        /*and and make the current item more visible:*/
-        addActive(x);
-        } else if (e.keyCode == 13) {
-        /*If the ENTER key is pressed, prevent the form from being submitted,*/
-        e.preventDefault();
-        if (currentFocus > -1) {
-            /*and simulate a click on the "active" item:*/
-            if (x) x[currentFocus].click();
-        }
-        }
-    });
-    function addActive(x) {
-    /*a function to classify an item as "active":*/
-    if (!x) return false;
-    /*start by removing the "active" class on all items:*/
-    removeActive(x);
-    if (currentFocus >= x.length) currentFocus = 0;
-    if (currentFocus < 0) currentFocus = (x.length - 1);
-    /*add class "autocomplete-active":*/
-    x[currentFocus].classList.add("autocomplete-active");
-    }
-    function removeActive(x) {
-    /*a function to remove the "active" class from all autocomplete items:*/
-    for (var i = 0; i < x.length; i++) {
-        x[i].classList.remove("autocomplete-active");
-    }
-    }
-    function closeAllLists(elmnt) {
-    /*close all autocomplete lists in the document,
-    except the one passed as an argument:*/
-    var x = document.getElementsByClassName("autocomplete-items");
-    for (var i = 0; i < x.length; i++) {
-        if (elmnt != x[i] && elmnt != inp) {
-        x[i].parentNode.removeChild(x[i]);
-        }
-    }
-    }
-    /*execute a function when someone clicks in the document:*/
-    document.addEventListener("click", function (e) {
-        closeAllLists(e.target);
-    });
-}
-
 function simulateKey(elementId, key) {
     const element = document.getElementById(elementId);
 
@@ -695,22 +581,5 @@ function createInput(placeholder, a) {
 }
 
 function submitData() {
-    const userList = document.getElementById('user-list');
-    const userRows = userList.getElementsByClassName('user-row');
 
-    const userData = [];
-
-    for (let i = 0; i < userRows.length; i++) {
-        const userRow = userRows[i];
-        const name = userRow.children[0].value;
-        const age = userRow.children[1].value;
-        const city = userRow.children[2].value;
-
-        if (name && age && city) {
-            userData.push({ name, age, city });
-        }
-    }
-
-    // Aqui você pode fazer o que quiser com os dados, como enviar para o servidor
-    console.log('Dados enviados:', userData);
 }
