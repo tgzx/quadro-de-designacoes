5ed5c4c814e427b1f6d2a53f2ccab6f356395226
tgzx • Tiago Moreira <tiago.z.x.g@gmail.com>
2024-02-21 14:39:12 -0300

    Revert "testing db"
    This reverts commit 9176cb21d22e242cf0d6f37fc4b4932e39122172.

--- assignment-card.js
+++ assignment-card.js
@@ -42,7 +42,6 @@ function getActiveSection(){
 }
 
 function search() {
-    run();
     let name = document.getElementById('inputName').value;
         
     if (name !== "" && name !== undefined) {
@@ -88,34 +87,6 @@ function isTokenExpired(token) {
     // Compara a data atual com a data de expiração
     return currentTime >= expirationTime;
 }
-
-
-import { MongoClient, ServerApiVersion } from 'mongodb';
-const uri = "mongodb+srv://dbUser:Batata2020@cluster0.whcfjzc.mongodb.net/?retryWrites=true&w=majority";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
 
 
 function generateAccessToken() {
